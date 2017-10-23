ba15f3a3a6d0bddba39a16542d6f6720d1621b9f
Mike Rogers <me@mikerogers.io>
2017-10-23 15:42:34 +0200

    Fixing a few of then hound errors

--- test/units/injected/live_resource_test.js
+++ test/units/injected/live_resource_test.js
@@ -8,15 +8,15 @@ QUnit.module( "nonCacheURL", {
 } );
 
 QUnit.test( "Append the cache breaker when hard caching is enabled", function( assert ) {
-  window.$livePage = {}
-  window.$livePage.options = {}
+  window.$livePage = {};
+  window.$livePage.options = {};
 
-  window.$livePage.options.use_hard_cache_breaker = true
+  window.$livePage.options.use_hard_cache_breaker = true;
 
   assert.equal( new LiveResource('http://www.site.com/').nonCacheURL(), "http://www.site.com/"+ '?livePage=' + (new Date() * 1) );
   assert.equal( new LiveResource('http://www.site.com/?apple=true').nonCacheURL(), "http://www.site.com/?apple=true" + '&livePage=' + (new Date() * 1) );
 
-  window.$livePage.options.use_hard_cache_breaker = false
+  window.$livePage.options.use_hard_cache_breaker = false;
   assert.equal( new LiveResource('http://www.site.com/').nonCacheURL(), "http://www.site.com/" );
   assert.equal( new LiveResource('http://www.site.com/?apple=true').nonCacheURL(), "http://www.site.com/?apple=true" );
 });

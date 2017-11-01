2085c0acc610117e90cff2220d225e2a96748c6b
Mike Rogers <me@mikerogers.io>
2017-11-01 16:54:42 +0000

    Updating translator to run sync

--- tasks/translation.js
+++ tasks/translation.js
@@ -4,21 +4,32 @@ var enTranslation = JSON.parse(fs.readFileSync('../_locales/en/messages.json', '
 var enTranslationStoreDescription = JSON.parse(JSON.stringify({text: fs.readFileSync('../_locales/en/description_from_webstore.txt', 'utf8')}));
 
 TJO.init({
-  googleApiKey: 'api_key'
+  googleApiKey: 'AIzaSyBEvHECf9wV2tuO-qjigXgKW2TZQGdOWE0'
 });
 
 //var languages = ['af', 'am', 'ar', 'az', 'be', 'bg', 'bn', 'bs', 'ca', 'ceb', 'co', 'cs', 'cy', 'da', 'de', 'el', 'en', 'eo', 'es', 'et', 'eu', 'fa', 'fi', 'fr', 'fy', 'ga', 'gd', 'gl', 'gu', 'ha', 'haw', 'hi', 'hmn', 'hr', 'ht', 'hu', 'hy', 'id', 'ig', 'is', 'it', 'iw', 'ja', 'jw', 'ka', 'kk', 'km', 'kn', 'ko', 'ku', 'ky', 'la', 'lb', 'lo', 'lt', 'lv', 'ma', 'mg', 'mi', 'mk', 'ml', 'mn', 'mr', 'ms', 'mt', 'my', 'ne', 'nl', 'no', 'ny', 'pl', 'ps', 'pt', 'ro', 'ru', 'sd', 'si', 'sk', 'sl', 'sm', 'sn', 'so', 'sq', 'sr', 'st', 'su', 'sv', 'sw', 'ta', 'te', 'tg', 'th', 'tl', 'tr', 'uk', 'ur', 'uz', 'vi', 'xh', 'yi', 'yo', 'zh-CN', 'zh-TW', 'zu']
-var languages = ['de', 'es', 'fr', 'it', 'ja', 'pt', 'ru', 'ko', 'zh-CN', 'zh-TW']
+var languages = ['pt', 'ko', 'zh-CN', 'zh-TW']
 
-var targetLanguage = 'pt';
+function translateMessages(targetLanguage) {
+  return new Promise(resolve => {
+    TJO.translate(enTranslation, targetLanguage)
+      .then(function(data) {
+        if (!fs.existsSync('../_locales/' + targetLanguage)){
+          fs.mkdirSync('../_locales/' + targetLanguage);
+        }
+        fs.writeFileSync('../_locales/' + targetLanguage + '/messages.json', JSON.stringify(data, null, 2), 'utf8');
 
-// Uses promises to run all the languages one after the other.
-TJO.translate(enTranslation, targetLanguage)
-  .then(function(data) {
-    if (!fs.existsSync('../_locales/' + targetLanguage)){
-      fs.mkdirSync('../_locales/' + targetLanguage);
-    }
-    fs.writeFileSync('../_locales/' + targetLanguage + '/messages.json', JSON.stringify(data, null, 2), 'utf8');
-  }).catch(function(err) {
-    console.log('error ', err)
+        resolve();
+      }).catch(function(err) {
+        console.log('error ', err)
+      });
   });
+}
+
+async function main() {  
+  for(i in languages) {
+    console.log('Translating: ' + languages[i]);
+    await translateMessages(languages[i]);
+  }
+};
+main();

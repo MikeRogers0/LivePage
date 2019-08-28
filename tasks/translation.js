12071c9b1601bf37995333efb344682a78602a7d
Mike Rogers <me@mikerogers.io>
2019-08-28 20:41:18 +0100

    Cycling my API key out

--- tasks/translation.js
+++ tasks/translation.js
@@ -4,7 +4,8 @@ var enTranslation = JSON.parse(fs.readFileSync('../_locales/en/messages.json', '
 var enTranslationStoreDescription = JSON.parse(JSON.stringify({text: fs.readFileSync('../_locales/en/description_from_webstore.txt', 'utf8')}));
 
 TJO.init({
-  googleApiKey: 'AIzaSyBEvHECf9wV2tuO-qjigXgKW2TZQGdOWE0'
+  // Get a key from: https://console.cloud.google.com/apis/credentials
+  googleApiKey: ''
 });
 
 //var languages = ['af', 'am', 'ar', 'az', 'be', 'bg', 'bn', 'bs', 'ca', 'ceb', 'co', 'cs', 'cy', 'da', 'de', 'el', 'en', 'eo', 'es', 'et', 'eu', 'fa', 'fi', 'fr', 'fy', 'ga', 'gd', 'gl', 'gu', 'ha', 'haw', 'hi', 'hmn', 'hr', 'ht', 'hu', 'hy', 'id', 'ig', 'is', 'it', 'iw', 'ja', 'jw', 'ka', 'kk', 'km', 'kn', 'ko', 'ku', 'ky', 'la', 'lb', 'lo', 'lt', 'lv', 'ma', 'mg', 'mi', 'mk', 'ml', 'mn', 'mr', 'ms', 'mt', 'my', 'ne', 'nl', 'no', 'ny', 'pl', 'ps', 'pt', 'ro', 'ru', 'sd', 'si', 'sk', 'sl', 'sm', 'sn', 'so', 'sq', 'sr', 'st', 'su', 'sv', 'sw', 'ta', 'te', 'tg', 'th', 'tl', 'tr', 'uk', 'ur', 'uz', 'vi', 'xh', 'yi', 'yo', 'zh-CN', 'zh-TW', 'zu']
